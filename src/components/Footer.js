6b490de066b4c19bfb35e16ddd5ea9ab9c86327f
kurocraft0312 <kurocraft0312@gmail.com>
2019-11-13 13:56:56 +0900

    アイコン等を差し替え。

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import github from '../img/social/github.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -83,10 +83,10 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="vimeo" href="https://vimeo.com">
+                <a title="github" href="https://github.com/kurocraft0312">
                   <img
-                    src={vimeo}
-                    alt="Vimeo"
+                    src={github}
+                    alt="GitHub"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
